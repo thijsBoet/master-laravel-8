3436f1c900d370b0194c43ca2a1db96ac5efb248
Matthijs Boet <m.boet@chello.nl>
2021-09-19 19:34:31 +0200

    starting learning controllers, finished req/res

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -45,10 +46,12 @@ $posts = [
 ];
 
 Route::get('/posts', function () use ($posts) {
+    // dd(request()->all());
+    dd((int)request()->query('page', 1));
     // compact($posts) === ['posts'] => $posts
-    return view('posts.index', ['posts'=> $posts])->name('posts.show');
+    return view('posts.index', ['posts' => $posts]);
 });
-    
+
 Route::get('posts/{id}', function ($id)  use ($posts) {
     abort_if(!isset($posts[$id]), 404);
 
@@ -58,36 +61,36 @@ Route::get('posts/{id}', function ($id)  use ($posts) {
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Post from ' . $daysAgo . ' days ago.';
-})->name('posts.recent.index');
+})->name('posts.recent.index')->middleware('auth');
 
-Route::prefix('/fun')->name('fun.')->group(function() use($posts){
+Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
     Route::get('/responses', function () use ($posts) {
         return response($posts, 201)
             ->header('Content-Type', 'application/json')
             ->cookie('MY_COOKIE', 'Matthijs Boet', 3600);
-    })->;
+    })->name('responses');
 
     Route::get('/redirect', function () {
         return redirect('/contact');
-    });
+    })->name('redirect');
 
     Route::get('/back', function () {
         return back()->withInput();
-    });
+    })->name('back');
 
     Route::get('/named-route', function () {
         return redirect()->route('posts.show', ['id' => 1]);
-    });
+    })->name('named-route');
 
     Route::get('/away', function () {
         return redirect()->away('http://google.com');
-    });
+    })->name('away');
 
     Route::get('/json', function () use ($posts) {
         return response()->json($posts);
-    });
+    })->name('json');
 
     Route::get('/download', function () use ($posts) {
         return response()->download(public_path('/daniel.jpg'), 'milla-jovovich.jpg');
-    });
+    })->name('download');
 });
